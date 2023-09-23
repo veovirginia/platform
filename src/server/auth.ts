9b9f121719ac11d9b570a4e3cc897483da1a1f99
Tony <totran0331@gmail.com>
2023-09-23 13:13:31 -0400

    Email provider signin for next-auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,11 +4,16 @@ import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
+  type Session,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "@/env.mjs";
 import { db } from "@/server/db";
+import Email from "next-auth/providers/email";
+import { type AdapterUser } from "next-auth/adapters";
+import { type JWT } from "next-auth/jwt";
+import nodemailer from "nodemailer";
+import type SMTPTransport from "nodemailer/lib/smtp-transport";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -20,16 +25,134 @@ declare module "next-auth" {
   interface Session extends DefaultSession {
     user: DefaultSession["user"] & {
       id: string;
-      // ...other properties
-      // role: UserRole;
+      email: string;
+      emailVerified: Date | null;
+      onboarded: string;
+      name: string;
+      bio: string;
+      image: string;
+      phone: string;
+      graduation: string;
+      major: string;
+      idea: string;
     };
   }
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+  interface User {
+    id: string;
+    email: string;
+    emailVerified: Date | null;
+    onboarded: string;
+    name: string;
+    bio: string;
+    image: string;
+    phone: string;
+    graduation: string;
+    major: string;
+    idea: string;
+  }
 }
+
+export const SEVEN_DAYS_IN_SECONDS = 604800;
+
+function text({ url, host }: Record<"url" | "host", string>) {
+  const urlBuffer = Buffer.from(encodeURIComponent(url));
+  const base64URL = urlBuffer.toString("base64");
+  const emailURL = `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/access/${base64URL}`;
+  return `Sign in to ${host}\n${emailURL}\n\n`;
+}
+
+function html({ url, host, email }: Record<"url" | "host" | "email", string>) {
+  const escapedEmail = `${email.replace(/\./g, "&#8203;.")}`;
+  const escapedHost = `${host.replace(/\./g, "&#8203;.")}`;
+
+  const backgroundColor = "#030303";
+
+  const textColor = "#d4d4d8";
+  const mainBackgroundColor = "#121212";
+  const buttonBackgroundColor = "#fafafa";
+  const buttonBorderColor = "#fafafa";
+  const buttonTextColor = "#030303";
+
+  const urlBuffer = Buffer.from(encodeURIComponent(url));
+  const base64URL = urlBuffer.toString("base64");
+  const emailURL = `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/access/${base64URL}`;
+
+  return `
+  <body style="background: ${backgroundColor};">
+ <table width="100%" border="0" cellspacing="0" cellpadding="0">
+   <tr>
+     <td align="center" style="padding: 10px 0px 20px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+       <strong>${escapedHost}</strong>
+     </td>
+   </tr>
+ </table>
+ <table width="100%" border="0" cellspacing="20" cellpadding="0" style="background: ${mainBackgroundColor}; max-width: 600px; margin: auto; border-radius: 10px;">
+   <tr>
+     <td align="center" style="padding: 10px 0px 0px 0px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+       Sign in as <strong>${escapedEmail}</strong>
+     </td>
+   </tr>
+   <tr>
+     <td align="center" style="padding: 20px 0;">
+       <table border="0" cellspacing="0" cellpadding="0">
+         <tr>
+           <td align="center" style="border-radius: 5px;" bgcolor="${buttonBackgroundColor}"><a href="${emailURL}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${buttonBorderColor}; display: inline-block; font-weight: bold;">Sign in</a></td>
+         </tr>
+       </table>
+     </td>
+   </tr>
+   <tr>
+     <td align="center" style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+       If you did not request this email you can safely ignore it.
+     </td>
+   </tr>
+ </table>
+</body>
+`;
+}
+
+const EmailProvider = Email({
+  server: {
+    host: env.EMAIL_HOST,
+    port: Number(env.EMAIL_PORT),
+    auth: {
+      user: env.EMAIL_USER,
+      pass: env.EMAIL_PASSWORD,
+    },
+  },
+  from: `Virginia Entrepreneurship Organization <${env.EMAIL_FROM}>`,
+  async sendVerificationRequest({
+    identifier: email,
+    url,
+    provider: { server, from },
+  }) {
+    const { host } = new URL(url);
+    const transport = nodemailer.createTransport(server as SMTPTransport);
+    await transport.sendMail({
+      to: email,
+      from,
+      subject: `Sign in to ${host}`,
+      text: text({ url, host }),
+      html: html({ url, host, email }),
+    });
+  },
+});
+
+interface SessionCallbackParams {
+  session: Session;
+  token: JWT;
+  user: AdapterUser;
+  newSession: unknown;
+  trigger: "update";
+}
+
+const session = ({ session, user }: SessionCallbackParams) => {
+  if (session.user) {
+    session.user = { ...user };
+  }
+  return session;
+};
 
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
@@ -38,30 +161,20 @@ declare module "next-auth" {
  */
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
+    session,
   },
   adapter: PrismaAdapter(db),
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
+  providers: [EmailProvider],
+  pages: {
+    signIn: "/signin",
+    newUser: "/onboard",
+  },
+  secret: env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "database",
+    maxAge: SEVEN_DAYS_IN_SECONDS,
+    updateAge: SEVEN_DAYS_IN_SECONDS,
+  },
 };
 
 /**
