ce579252ce9b1b7e3994c33d582f0befa7d532e3
Tony <totran0331@gmail.com>
2023-10-04 21:13:55 -0400

    Refactored form steps

--- src/components/forms/OnboardStepOneForm.tsx
+++ src/components/forms/OnboardStepOneForm.tsx
@@ -41,7 +41,6 @@ const formSchema = z.object({
 });
 
 const OnboardStepOneForm: FC = () => {
-  const [step, setStep] = useState<number>(0);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -58,131 +57,88 @@ const OnboardStepOneForm: FC = () => {
   };
 
   return (
-    <div className="w-full max-w-2xl">
-      <div className="pb-12">
-        <HeadingTwo className="text-center">Almost there!</HeadingTwo>
-        <div className="pt-2">
-          <Paragraph className="text-center leading-4 text-muted-foreground">
-            Complete the form to finalize your membership.
-          </Paragraph>
+    <Form {...form}>
+      <form
+        onSubmit={(event) => void form.handleSubmit(onSubmit)(event)}
+        className="grid grid-cols-4 gap-4 rounded border-muted"
+      >
+        <div className="col-span-2">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Full Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
-      </div>
-      <Form {...form}>
-        <div className="grid grid-cols-3 gap-2 pb-2 text-sm text-muted-foreground">
-          <div>
-            <p className="">
-              1.{" "}
-              <span className={cn("text-neutral-200")}>
-                Tell us about yourself
-              </span>
-            </p>
-          </div>
-          <div>
-            <p className="">2. Schedule a coffee chat</p>
-          </div>
-          <div>
-            <p className="">3. Await verification</p>
-          </div>
+        <div className="col-span-2">
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone number</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
-        <div className="grid grid-cols-3 gap-2 pb-4">
-          <div className="h-1 w-full rounded-full bg-red-500" />
-          <div className="h-1 w-full rounded-full bg-red-500" />
-          <div className="h-1 w-full rounded-full  bg-red-500" />
+        <div className="col-span-2">
+          <FormField
+            control={form.control}
+            name="graduation"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Expected graduation</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
-        <form
-          onSubmit={(event) => void form.handleSubmit(onSubmit)(event)}
-          className="grid grid-cols-4 gap-4 rounded border border-muted p-4"
-        >
-          <div className="col-span-2">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Full Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="John Doe" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="col-span-2">
-            <FormField
-              control={form.control}
-              name="phone"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Phone number</FormLabel>
-                  <FormControl>
-                    <Input placeholder="John Doe" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="col-span-2">
-            <FormField
-              control={form.control}
-              name="graduation"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Expected graduation</FormLabel>
-                  <FormControl>
-                    <Input placeholder="John Doe" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="col-span-2">
-            <FormField
-              control={form.control}
-              name="major"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Major</FormLabel>
-                  <FormControl>
-                    <Input placeholder="John Doe" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="col-span-4">
-            <FormField
-              control={form.control}
-              name="idea"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Idea</FormLabel>
-                  <FormControl>
-                    <Input placeholder="The next big thing..." {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="col-span-4 flex items-center justify-end gap-4 pt-4">
-            <div className="">
-              <Button variant="ghost" type="submit">
-                Previous
-              </Button>
-            </div>
-            <div className="">
-              <Button variant="default" type="submit">
-                Continue
-              </Button>
-            </div>
-          </div>
-        </form>
-      </Form>
-    </div>
+        <div className="col-span-2">
+          <FormField
+            control={form.control}
+            name="major"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Major</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="col-span-4">
+          <FormField
+            control={form.control}
+            name="idea"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Idea</FormLabel>
+                <FormControl>
+                  <Input placeholder="The next big thing..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
+    </Form>
   );
 };
 
--- src/pages/platform/onboard/index.tsx
+++ src/pages/platform/onboard/index.tsx
@@ -1,13 +1,103 @@
 import OnboardStepOneForm from "@/components/forms/OnboardStepOneForm";
 import DefaultLayout from "@/components/layouts/DefaultLayout";
+import { Button } from "@/components/ui/button";
+import HeadingTwo from "@/components/ui/headingTwo";
+import Paragraph from "@/components/ui/paragraph";
+import { cn } from "@/lib/utils";
 import { type NextPage, type GetServerSidePropsContext } from "next";
 import { getSession } from "next-auth/react";
+import { useState } from "react";
 
 const Onboard: NextPage = () => {
+  const [step, setStep] = useState<number>(0);
+
+  const nextPage = () => {
+    setStep((prev) => (prev += 1));
+  };
+
+  const prevPage = () => {
+    setStep((prev) => (prev -= 1));
+  };
   return (
     <DefaultLayout>
-      <div className="flex w-full flex-1 justify-center pt-12">
-        <OnboardStepOneForm />
+      <div className="flex w-full flex-1 justify-center pt-16">
+        <div className="w-full max-w-2xl">
+          <div className="pb-12">
+            <HeadingTwo className="text-center">Almost there!</HeadingTwo>
+            <div className="pt-2">
+              <Paragraph className="text-center leading-4 text-muted-foreground">
+                Complete the form to finalize your membership.
+              </Paragraph>
+            </div>
+          </div>
+          <div className="grid grid-cols-3 gap-2 pb-2 text-sm text-muted-foreground">
+            <div>
+              <p className="">
+                1.{" "}
+                <span
+                  className={cn({ "font-medium text-[#08D9D6]": step === 0 })}
+                >
+                  Tell us about yourself
+                </span>
+              </p>
+            </div>
+            <div>
+              <p className="">
+                2.{" "}
+                <span
+                  className={cn({ "font-medium text-[#30E3CA]": step === 1 })}
+                >
+                  Schedule a coffee chat
+                </span>
+              </p>
+            </div>
+            <div>
+              <p className="">
+                3.{" "}
+                <span
+                  className={cn({ "font-medium text-[#30E3CA]": step === 1 })}
+                >
+                  Await verification
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="grid grid-cols-3 gap-2 pb-8">
+            <div
+              className={cn("h-1 w-full rounded-full", {
+                "bg-[#30E3CA]": step > 0,
+                "bg-muted": step <= 0,
+              })}
+            />
+            <div
+              className={cn("h-1 w-full rounded-full", {
+                "bg-[#30E3CA]": step > 1,
+                "bg-muted": step <= 1,
+              })}
+            />
+            <div
+              className={cn("h-1 w-full rounded-full", {
+                "bg-[#30E3CA]": step > 2,
+                "bg-muted": step <= 2,
+              })}
+            />
+          </div>
+          <div className="rounded border border-muted p-6">
+            <OnboardStepOneForm />
+            <div className="flex w-full items-center justify-end gap-4 pt-8">
+              <div className="">
+                <Button variant="ghost" type="submit">
+                  Previous
+                </Button>
+              </div>
+              <div className="">
+                <Button variant="default" onClick={() => nextPage()}>
+                  Continue
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </DefaultLayout>
   );
@@ -16,23 +106,23 @@ const Onboard: NextPage = () => {
 export default Onboard;
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
-  const session = await getSession(ctx);
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/signin",
-        permanent: true,
-      },
-    };
-  }
-  if (session.user.onboarded) {
-    return {
-      redirect: {
-        destination: "/platform",
-        permanent: true,
-      },
-    };
-  }
+  // const session = await getSession(ctx);
+  // if (!session) {
+  //   return {
+  //     redirect: {
+  //       destination: "/signin",
+  //       permanent: true,
+  //     },
+  //   };
+  // }
+  // if (session.user.onboarded) {
+  //   return {
+  //     redirect: {
+  //       destination: "/platform",
+  //       permanent: true,
+  //     },
+  //   };
+  // }
   return {
     props: {},
   };
