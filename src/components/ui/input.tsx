424ceb30efa924d49ffea380480b3ecab766a3c5
Tony <totran0331@gmail.com>
2023-10-06 00:56:25 -0400

    Added phone input and pattern input components

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,5 +1,11 @@
 import * as React from "react";
-
+import PhoneNumberInput from "react-phone-number-input/react-hook-form-input";
+import type { E164Number } from "libphonenumber-js/core";
+import {
+  type InputAttributes,
+  PatternFormat,
+  type PatternFormatProps,
+} from "react-number-format";
 import { cn } from "@/lib/utils";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
@@ -20,4 +26,46 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 );
 Input.displayName = "Input";
 
-export { Input };
+export type PhoneInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  value: E164Number;
+};
+
+const PhoneInput = React.forwardRef<HTMLInputElement, PhoneInputProps>(
+  ({ className, value, ...props }, ref) => {
+    return (
+      <PhoneNumberInput
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        defaultCountry="US"
+        value={value}
+        name={props.name ?? ""}
+        placeholder="+## (###) ###-####"
+      />
+    );
+  },
+);
+PhoneInput.displayName = "PhoneInput";
+
+export type PatternInputProps = PatternFormatProps<InputAttributes> & {
+  className?: string;
+};
+
+const PatternInput = React.forwardRef<HTMLInputElement, PatternInputProps>(
+  ({ className, ...props }) => {
+    return (
+      <PatternFormat
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        {...props}
+      />
+    );
+  },
+);
+PatternInput.displayName = "PatternInput";
+
+export { Input, PhoneInput, PatternInput };
