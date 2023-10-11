ff77fe51959a5a3418873f586d564faf5e6b3eac
Tony <totran0331@gmail.com>
2023-10-11 02:28:24 -0400

    Persist step one form value across onboarding

--- src/components/atoms/onboardFormAtom.ts
+++ src/components/atoms/onboardFormAtom.ts
@@ -1,8 +1,15 @@
 import { type OnboardStepOneValues } from "@/lib/types";
 import { atom } from "jotai";
+import { type EmptyObject } from "type-fest";
 
 export const stepAtom = atom<number>(1);
 
 export const validStepOneAtom = atom<boolean>(false);
 
-export const stepOneValuesAtom = atom<OnboardStepOneValues | null>(null);
+export const stepOneValuesAtom = atom<OnboardStepOneValues>({
+  name: "",
+  phone: "",
+  graduation: "",
+  major: "",
+  idea: "",
+});
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -31,19 +31,22 @@ export type PhoneInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 };
 
 const PhoneInput = React.forwardRef<HTMLInputElement, PhoneInputProps>(
-  ({ className, value, ...props }, ref) => {
+  ({ className, value, onBlur, ...props }, ref) => {
     return (
-      <PhoneNumberInput
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className,
-        )}
-        ref={ref}
-        defaultCountry="US"
-        value={value}
-        name={props.name ?? ""}
-        placeholder="+## (###) ###-####"
-      />
+      <>
+        <PhoneNumberInput
+          className={cn(
+            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            className,
+          )}
+          ref={ref}
+          defaultCountry="US"
+          value={value}
+          name={props.name ?? ""}
+          placeholder="+## (###) ###-####"
+          onBlur={onBlur}
+        />
+      </>
     );
   },
 );
@@ -54,7 +57,7 @@ export type PatternInputProps = PatternFormatProps<InputAttributes> & {
 };
 
 const PatternInput = React.forwardRef<HTMLInputElement, PatternInputProps>(
-  ({ className, ...props }) => {
+  ({ className, ...props }, _ref) => {
     return (
       <PatternFormat
         className={cn(
